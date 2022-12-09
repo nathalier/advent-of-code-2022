b5e5c0ec020a45d742158e1370b192a3cfee1e52
nathalier <nathalier@gmail.com>
2022-12-09 15:04:40 +0000

    add optimization for part 1 into general case

--- day_09/day_09.py
+++ day_09/day_09.py
@@ -4,12 +4,12 @@ import numpy as np
 
 def trace_tail(moves, knots_num):
     knot_positions = [np.array([0, 0])] * (knots_num + 1)
-    tail_visited = set()
-    tail_visited.add((0, 0))
+    tail_visited = set([(0, 0)])
 
     for direction, steps in moves:
-        for _ in range(steps):
-            knot_positions[0] = knot_positions[0] + 1 * direction
+        steps_in_one_go = 1 if knots_num > 1 else steps
+        for _ in range(steps // steps_in_one_go):
+            knot_positions[0] = knot_positions[0] + steps_in_one_go * np.array(direction)
             for i in range(len(knot_positions) - 1):
                 tail_pos, newly_visited = adjust_tail(knot_positions[i], knot_positions[i+1]) 
                 knot_positions[i+1] = tail_pos
@@ -32,19 +32,7 @@ def adjust_tail(head_pos, tail_pos):
 
 
 def part_1(data):
-    # return trace_tail(data, 1)  # less optimal
-
-    head_pos, tail_pos = np.array([0, 0]), np.array([0, 0])
-    tail_visited = set()
-    tail_visited.add(((0, 0)))
-
-    for move in data:
-        head_move_vector = np.array(move[0]) * move[1]
-        head_pos = head_pos + head_move_vector
-        tail_pos, newly_visited = adjust_tail(head_pos, tail_pos) 
-        tail_visited.update(newly_visited)
-
-    return len(tail_visited)
+    return trace_tail(data, 1)  
 
 
 def part_2(data):
